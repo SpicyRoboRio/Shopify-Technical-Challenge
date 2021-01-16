f53e35e9741e266ba75616acba1982dd136f7d74
Shaquille Sadiq <ayobamissadiq@gmail.com>
2021-01-16 18:08:26 -0500

    Added XHTMLResuest to GET data from API

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,16 @@
-apiKey = "ec136557";
+let apiKey = "ec136557";
+
+let request = new XMLHttpRequest();
 
 
 function apiRequest(reqParam){ //?i=tt3896198
-    return "http://www.omdbapi.com/" + reqParam + "&apikey=" + apiKey + "";
+    requestURL = "http://www.omdbapi.com/" + reqParam + "&apikey=" + apiKey + "";
+    request.open("GET", requestURL);
+    request.send();
+    request.onload = () =>{
+        console.log(request);
+        if(request.status === 200){
+            console.log(JSON.parse(request.response));
+        }
+    }
 } 

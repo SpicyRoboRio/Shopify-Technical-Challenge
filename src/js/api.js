21d4a6e463890c4ac85ec5703fa7295c3221e1f7
Shaquille Sadiq <ayobamissadiq@gmail.com>
2021-01-17 02:42:37 -0500

    added nomination and denomination functionality

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,7 @@
 let apiKey = "ec136557";
 
 let request = new XMLHttpRequest();
+let nomList = [];
 
 //Send APIRequest to retrieve movie data
 function apiRequest(reqParam, handleData){
@@ -24,16 +25,16 @@ function apiRequest(reqParam, handleData){
 
 //Search OMDB for movies by title
 function searchMovieByTitle(movieTitle){
-    apiRequest("?s=" + movieTitle, handleAPIData);
+    apiRequest("?s=" + movieTitle, displaySearchData);
 }
 
 //Search OMDB for movies by ID
 function searchMovieByID(movieID){
-    apiRequest("?i=" + movieID, handleAPIData);
+    apiRequest("?i=" + movieID, displaySearchData);
 }
 
 // handle data retrieved from OMDB DB and display movie title and name(API Request Callback)
-function handleAPIData(reqResults){
+function displaySearchData(reqResults){
     console.log("HANDLING DATA:");
     console.log(reqResults);
 
@@ -41,12 +42,12 @@ function handleAPIData(reqResults){
         if(reqResults["Search"] !== undefined){
             for(i = 0; i < reqResults["Search"].length; i++){
                 currResult = reqResults["Search"][i];
-                addSearchResult(currResult["Title"], currResult["Year"], i);
+                addSearchResult(currResult["Title"], currResult["Year"], currResult["imdbID"], i);
             }
         
         }
         else{
-            addSearchResult(reqResults["Title"], reqResults["Year"], 0);
+            addSearchResult(reqResults["Title"], reqResults["Year"], reqResults["imdbID"], 0);
         }
     }
     else if(reqResults["Response"] == "False"){
@@ -59,14 +60,66 @@ function handleAPIData(reqResults){
     }
 }
 
-function addSearchResult(movieTitle, movieYear, num){
-    let searchResCont = "<div id='movie'" + num + ">\
-                            <p id='movieTitle'>" + movieTitle + "</p>\
-                            <br>\
-                            <p id='movieYear'>" + movieYear + "</p>\
-                            <br>\
-                            <input id='nominateBtn' type='button' value='Nominate'>\
-                        </div>";
+function addSearchResult(movieTitle, movieYear, imdbID, num){
+    let searchResCont = "<form id='nominateForm' ref='nominateForm'>\
+                            <div id='movie" + num + "' imdbID='" + imdbID + "'>\
+                                <p id='movieTitle'>" + movieTitle + "</p>\
+                                <p id='movieYear'>" + movieYear + "</p>\
+                                <div  class='form-group'></div>\
+                                    <input class='nominateBtn" + num + "' type='button' value='Nominate'>\
+                                </div>\
+                            </div>\
+                        </form><br>";
 
     $('#searchResults').append(searchResCont);
+    document.getElementById("nominateBtn" + num).onclick = function(m, movieID=imdbID){
+        if(nomList.length >= 5){
+            alert("Max of 5 Nominations!");
+        }
+        else{
+            addNomination(movieID);
+        }
+    };
+}
+
+function addNomination(movieID){
+    apiRequest("?i=" + movieID, getNominatedMovie);
+}
+
+function getNominatedMovie(reqResults){
+    console.log("HANDLING DATA:");
+    console.log(reqResults);
+
+    if(reqResults["Response"] != "False"){
+        nomList.push(reqResults);
+        displayNominatedMovie();
+    }
+    else if(reqResults["Response"] == "False"){
+        console.log(reqResults["Error"]);
+        alert(reqResults["Error"]);
+    }
+    else{
+        console.log("Error Getting Data");
+        alert("Error Nominating Movie");
+    }
+}
+
+function displayNominatedMovie(movieID){
+    let nomListcont = "<div id='" + movieID + "' imdbID='" + movieID + "'>\
+                            <p id='movieTitle'>" + movieTitle + "</p>\
+                            <p id='movieYear'>" + movieYear + "</p>\
+                            <input id='denominateBtn" + movieID + "' type='button' value='Remove'>\
+                        </div>\
+                        <br>";
+
+    $('#nominationList').append(nomListcont);
+
+    document.getElementById("denominateBtn" + movieID).onclick = function(m, movieID=imdbID){
+        for(i = 0; i < nomList.length; i++){ 
+            if(nomList[imdbID] === movieID){ 
+                arr.splice(i, 1); 
+            }
+        }
+        document.getElementById("denominateBtn" + movieID).remove();
+    };
 }
